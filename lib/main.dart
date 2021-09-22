1b8cf5d6ba94cf1928cce9d41dde13adbd31fe8a
unknown <ar.shah192@gmail.com>
2021-09-22 20:13:02 +0500

    test

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Material(
+            child: Center(
+      child: Container(
+        child: Text("Welcome to the First Flutter App"),
+      ),
+    )));
+  }
+}
